ea4aae63f1f9d6a49f80dd7b5e81137b368a6144
rexhens <rexhensb3@gmail.com>
2025-01-23 00:07:43 +0100

    System tests

--- Library-Management-System-main/Library-Management-System-main/LibraryManagementSystem/src/main/java/Views/AddAuthorView.java
+++ Library-Management-System-main/Library-Management-System-main/LibraryManagementSystem/src/main/java/Views/AddAuthorView.java
@@ -31,27 +31,37 @@ public class AddAuthorView {
         p1.getChildren().add(title);
         p1.setPadding(new Insets(20));
         bp.setTop(p1);
+
         // Title
         GridPane gp = new GridPane();
         gp.setHgap(10);
         gp.setVgap(10);
         gp.setPadding(new Insets(10, 10, 10, 10));
         gp.setAlignment(Pos.CENTER);
+
+        // Name field
         Label name = new Label("Name");
         TextField nameT = new TextField();
+        nameT.setId("nameT");  // Set ID for the name field
         gp.add(name, 0, 0);
         gp.add(nameT, 1, 0);
 
+        // Surname field
         Label surname = new Label("Surname");
         TextField surnameT = new TextField();
+        surnameT.setId("surnameT");  // Set ID for the surname field
         gp.add(surname, 0, 1);
         gp.add(surnameT, 1, 1);
 
+        // Gender selection
         Label gl = new Label("Gender");
         ToggleGroup tg = new ToggleGroup();
         RadioButton M = new RadioButton("Male");
+        M.setId("maleRadio");  // Set ID for Male radio button
         RadioButton F = new RadioButton("Female");
+        F.setId("femaleRadio");  // Set ID for Female radio button
         RadioButton O = new RadioButton("Other");
+        O.setId("otherRadio");  // Set ID for Other radio button
         M.setToggleGroup(tg);
         F.setToggleGroup(tg);
         O.setToggleGroup(tg);
@@ -60,13 +70,17 @@ public class AddAuthorView {
         gp.add(gl, 0, 2);
         gp.add(b1, 1, 2);
 
+        // System label and error message
         Label systemLabel = new Label("System");
-        Label label1=new Label("");
+        Label label1 = new Label("");
+        label1.setId("systemField");  // Set ID for error message label
         label1.setTextFill(Color.RED);
         gp.add(systemLabel, 0, 3);
         gp.add(label1, 1, 3);
 
+        // Register button
         Button registerButton = new Button("Register Author");
+        registerButton.setId("registerButton");  // Set ID for Register button
         registerButton.setOnAction(e -> {
             AuthorController controller = new AuthorController();
             Gender gender;
@@ -84,19 +98,25 @@ public class AddAuthorView {
                 success.showAndWait();
                 EmployeeHomePage employeeHomePage = new EmployeeHomePage(currentUser);
                 stage.setScene(employeeHomePage.showView(stage));
-            }else {
+            } else {
                 label1.setText(added.getErrorMessage());
             }
         });
+
+        // Back button
         Button back = new Button("Back");
+        back.setId("backButton");  // Set ID for Back button
         back.setOnAction(e -> {
             EmployeeHomePage employeeHomePage = new EmployeeHomePage(currentUser);
             stage.setScene(employeeHomePage.showView(stage));
         });
+
+        // Buttons container
         HBox b2 = new HBox();
         b2.setSpacing(10);
         b2.getChildren().addAll(registerButton, back);
         gp.add(b2, 1, 4);
+
         bp.setCenter(gp);
         return new Scene(bp, 700, 500);
     }
