b5924a088a78ba3b1df63bc529fc88d7462298ce
teameraj <129428019+teameraj@users.noreply.github.com>
2025-01-30 20:49:14 +0100

    Update LibrarianSystemTest.java

--- Library-Management-System-main/Library-Management-System-main/LibraryManagementSystem/src/test/java/views/LibrarianSystemTest.java
+++ Library-Management-System-main/Library-Management-System-main/LibraryManagementSystem/src/test/java/views/LibrarianSystemTest.java
@@ -1,4 +1,4 @@
-/*package views;
+package views;
 
 import Models.*;
 import Controllers.BillController;
@@ -149,4 +149,4 @@ public class LibrarianSystemTest extends ApplicationTest {
         assertTrue(lookup("#failedLabel").tryQuery().isPresent());
     }
 }
-*/
+
