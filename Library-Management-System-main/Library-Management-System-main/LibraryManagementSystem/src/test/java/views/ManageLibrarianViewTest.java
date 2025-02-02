9f77b285e17bfab3550435bc4161ae05d8c4cd46
teameraj <129428019+teameraj@users.noreply.github.com>
2025-02-02 13:31:59 +0100

    Update ManageLibrarianViewTest.java

--- Library-Management-System-main/Library-Management-System-main/LibraryManagementSystem/src/test/java/views/ManageLibrarianViewTest.java
+++ Library-Management-System-main/Library-Management-System-main/LibraryManagementSystem/src/test/java/views/ManageLibrarianViewTest.java
@@ -1,9 +1,11 @@
 package views;
+
 import Controllers.LibrarianController;
 import Models.Librarian;
 import Models.Roles;
 import Models.User;
 import Views.AdminHomePage;
+import Views.EditLibrarianView;
 import Views.EmployeeHomePage;
 import Views.ManageLibrarianView;
 import javafx.scene.layout.GridPane;
@@ -25,19 +27,25 @@ public class ManageLibrarianViewTest extends ApplicationTest {
     private User employeeUser;
     private Librarian librarian;
     private Stage stage;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        this.stage = stage;
+    }
+
     @BeforeEach
     public void setUp() {
         adminUser = mock(User.class);
         employeeUser = mock(User.class);
         librarian = mock(Librarian.class);
-        stage = mock(Stage.class);
         when(adminUser.getUserRole()).thenReturn(Roles.Admin);
         when(librarian.getName()).thenReturn("John");
         when(employeeUser.getUserRole()).thenReturn(Roles.Librarian);
         manageLibrarianView = new ManageLibrarianView(adminUser);
     }
+
     @Test
-    public void testSceneLoadsForAdmin() {
+    public void testSceneLoadsForAdmin() throws InterruptedException  {
         Scene scene = manageLibrarianView.showManageLibrarianView(librarian, stage);
         BorderPane borderPane = (BorderPane) scene.getRoot();
         StackPane topStackPane = (StackPane) borderPane.getTop();
@@ -54,9 +62,14 @@ public class ManageLibrarianViewTest extends ApplicationTest {
         assertNotNull(deleteButton);
         assertNotNull(performanceButton);
         assertNotNull(backButton);
+
+
+            Thread.sleep(1000);
+
     }
+
     @Test
-    public void testSceneLoadsForEmployee() {
+    public void testSceneLoadsForEmployee()  throws InterruptedException  {
         manageLibrarianView = new ManageLibrarianView(employeeUser);
         Scene scene = manageLibrarianView.showManageLibrarianView(librarian, stage);
         BorderPane borderPane = (BorderPane) scene.getRoot();
@@ -73,35 +86,53 @@ public class ManageLibrarianViewTest extends ApplicationTest {
         boolean hasDeleteButton = gridPane.getChildren().stream()
                 .anyMatch(node -> node instanceof Button && ((Button) node).getText().equals("Delete Librarian"));
         assertFalse(hasDeleteButton, "Employee should not have the 'Delete Librarian' button.");
+
+            Thread.sleep(1000);
     }
+
     @Test
-    public void testDeleteButtonAction() {
+    public void testDeleteButtonAction() throws InterruptedException  {
         LibrarianController librarianController = mock(LibrarianController.class);
         when(librarianController.deleteUserById(anyInt())).thenReturn(true);
         ManageLibrarianView manageLibrarianView = new ManageLibrarianView(adminUser);
         Scene scene = manageLibrarianView.showManageLibrarianView(librarian, stage);
         Button deleteButton = (Button) ((GridPane) scene.lookup(".grid-pane")).getChildren().get(2);
         deleteButton.fire();
+
+
+            Thread.sleep(1000);
+
+
         verify(librarianController, times(1)).deleteUserById(librarian.getId());
         AdminHomePage mockAdminHomePage = mock(AdminHomePage.class);
         when(mockAdminHomePage.manageLibrariansView(stage)).thenReturn(mock(Scene.class));
         verify(mockAdminHomePage, times(1)).manageLibrariansView(stage);
     }
+
     @Test
-    public void testBackButtonActionForAdmin() {
+    public void testBackButtonActionForAdmin() throws InterruptedException  {
         Scene scene = manageLibrarianView.showManageLibrarianView(librarian, stage);
         Button backButton = (Button) ((GridPane) scene.lookup(".grid-pane")).getChildren().get(3);
         backButton.fire();
+
+
+            Thread.sleep(1000);
+
         AdminHomePage adminHomePage = mock(AdminHomePage.class);
         when(adminHomePage.manageLibrariansView(stage)).thenReturn(new Scene(new BorderPane()));
         assertNotNull(adminHomePage.manageLibrariansView(stage));
     }
+
     @Test
-    public void testBackButtonActionForEmployee() {
+    public void testBackButtonActionForEmployee() throws InterruptedException   {
         manageLibrarianView = new ManageLibrarianView(employeeUser);
         Scene scene = manageLibrarianView.showManageLibrarianView(librarian, stage);
         Button backButton = (Button) ((GridPane) scene.lookup(".grid-pane")).getChildren().get(1);
         backButton.fire();
+
+            Thread.sleep(1000);
+
+
         EmployeeHomePage employeeHomePage = mock(EmployeeHomePage.class);
         when(employeeHomePage.showView(stage)).thenReturn(new Scene(new BorderPane()));
         assertNotNull(employeeHomePage.showView(stage));
